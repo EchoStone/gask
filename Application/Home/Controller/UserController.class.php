8406202925031afb00595423d52a45d5174847c8
ffuyfd <ffuyfd@163.com>
2017-09-29 16:33:56 +0800

    用户修改,用户标签

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -1,14 +1,60 @@
 <?php
 namespace Home\Controller;
 use Home\Logic\UserLogic;
+use Home\Logic\UserTagLogic;
 
 class UserController extends BaseController {
     private $logic;
+    private $tagLogic;
+    private $userId;
     public function __construct(){
+        $this->userId = 2;//session("userID");
         $this->logic = new UserLogic();
+        $this->tagLogic = new UserTagLogic();
     }
     public function userList(){
         $list = $this->logic->getUserList();
         $this->jsonReturn($list);
+    }
+
+    public function userUpdate(){
+        $brief = I("brief");
+        $askprice= I("askprice");
+
+        if(!$brief || !$askprice){
+            $this->jsonReturn(null, 999, "参数不能为空");
+        }
+
+        $upresult = $this->logic->updateUser($this->userId, $brief, $askprice);
+        if($upresult){
+            $this->jsonReturn("");
+        } else {
+            $this->jsonReturn(null, 998, "更新失败");
+        }
+
+    }
+
+    public function addUserFlag(){
+        $flag = I("flag");
+        if($flag === ""){
+            $this->jsonReturn(null, 999, "参数不能为空");
+        }
+        if($this->tagLogic->addTag($this->userId, $flag)){
+            $this->jsonReturn("");
+        } else {
+            $this->jsonReturn(null, 997, "添加失败");
+        }
+    }
+
+    public function delUserFlag(){
+        $flagId = I("flagId");
+        if(empty($flagId)){
+            $this->jsonReturn(null, 999, "参数不能为空");
+        }
+        if($this->tagLogic->delTag($this->userId, $flagId)){
+            $this->jsonReturn("");
+        } else {
+            $this->jsonReturn(null, 996, "删除失败");
+        }
     }
 }
--- Application/Home/Logic/UserLogic.class.php
+++ Application/Home/Logic/UserLogic.class.php
@@ -1,16 +1,43 @@
 <?php
 namespace Home\Logic;
 use  Home\Model\UserModel;
-
+use  Home\Model\UserTagModel;
 class UserLogic{
     private $modelHandel;
+    private $tagmodelHandel;
     public function __construct(){
         $this->modelHandel = new UserModel();
+        $this->tagmodelHandel = new UserTagModel();
     }
     public function getUserList()
     {
         $map = [];
         $list =  $this->modelHandel->getAllByCondition($map, '', "id desc");
-        return $list;
+        $ids = [];
+        foreach($list as $userInfo){
+            array_push($ids, $userInfo["id"]);
+        }
+        $newlist = [];
+        if($ids){
+            $tagList = $this->tagmodelHandel->getTagListByIds($ids);
+
+            foreach($list as $userInfo){
+                $newlist[$userInfo['id']] = $userInfo;
+                array_push($ids, $userInfo["id"]);
+            }
+
+            foreach($tagList as $tag){
+                $newlist[$tag["user_id"]]['tag'][] = $tag['tag'];
+            }
+        }
+
+        return $newlist;
+
+    }
+
+    public function updateUser($userId,$brief, $askprice){
+        $map = ["id" => $userId];
+        $data = ["brief" => $brief, "ask_price" => $askprice];
+        return $this->modelHandel->update($map, $data);
     }
 }
